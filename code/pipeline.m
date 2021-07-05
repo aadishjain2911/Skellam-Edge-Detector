c6232d1966efd26de7ba214e9d843859d865ddf6
Aadish Jain <66255546+aadishjain2911@users.noreply.github.com>
2021-07-05 19:26:21 +0530

    Comments Added

--- code/pipeline.m
+++ code/pipeline.m
@@ -3,9 +3,13 @@ N = 300 ;
 T = 300 ;
 fps = 20 ;
 img_path = 'orig5.png' ;
+
+% For Natural Images
 noisy_img = imread(img_path) ;
 
-% [img, noisy_img] = create_img(M, N, img_path) ;
+% For Synthetic Images
+[img, noisy_img] = create_img(M, N, img_path) ;
+
 edge_img = edge_det_img(noisy_img) ;
 
 figure(1);
@@ -20,6 +24,7 @@ imshow(edge(noisy_img, 'canny')) ;
 vid_path = './sample_videos/syn_vid1.avi';
 output_path = 'syn1_out.avi';
 
+% For Synthetic Video
 [frames, vid] = create_vid(M, N, T, vid_path);
 fg_frames = bg_subtract(frames, M, N) ;
 figure(1);
@@ -27,18 +32,19 @@ imshow(frames(:,:,1)./256) ;
 figure(2);
 imshow(fg_frames(:,:,1)) ;
 
-% [frames, object] = read_frames(vid_path) ;
-% M = object.Height ;
-% N = object.Width ;
-% T = object.numFrames ;
-% fr = zeros(M, N, T) ;
-% for i=1:T
-%     fr(:, :, i) = reshape(frames(:,i), M, N) ; 
-% end
-% fg_frames = bg_subtract(fr, M, N) ;
-% figure(1) ;
-% imshow(fr(:,:,1)./256) ;
-% figure(2) ;
-% imshow(fg_frames(:,:,1)) ;
+% For Natural Video
+[frames, object] = read_frames(vid_path) ;
+M = object.Height ;
+N = object.Width ;
+T = object.numFrames ;
+fr = zeros(M, N, T) ;
+for i=1:T
+    fr(:, :, i) = reshape(frames(:,i), M, N) ; 
+end
+fg_frames = bg_subtract(fr, M, N) ;
+figure(1) ;
+imshow(fr(:,:,1)./256) ;
+figure(2) ;
+imshow(fg_frames(:,:,1)) ;
 
 vid = write_vid(output_path, fps, fg_frames) ;
